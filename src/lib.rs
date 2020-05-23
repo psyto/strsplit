1b7a1ac14aaaddddff4e2635765203a229f74db5
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-05-23 15:54:55 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//!
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+pub struct StrSplit {}
+
+impl StrSplit {
+    pub fn new(haystack: &str, delimiter: &str) -> Self {}
+}
